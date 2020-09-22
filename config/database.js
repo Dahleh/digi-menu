178d6a2ad239fa1a63aaceda39853a8a2c966778
Faris Dahleh <farisdahleh@live.com>
2020-09-22 14:28:13 +0300

    Changes to my-project noted

--- config/database.js
+++ config/database.js
@@ -20,3 +20,18 @@ module.exports = ({ env }) => ({
     },
   },
 });
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'bookshelf',
+//       settings: {
+//         client: 'sqlite',
+//         filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+//       },
+//       options: {
+//         useNullAsDefault: true,
+//       },
+//     },
+//   },
+// });
