223934f449ad796732a3e81bb7426b8ba362a89e
Faris Dahleh <farisdahleh@live.com>
2020-09-22 13:17:24 +0300

    Initial Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a3771039907a5b9c2808e0a534f83fde'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7ad14039-ba9c-48d9-a0e5-bb902e8c17fc'
+};
